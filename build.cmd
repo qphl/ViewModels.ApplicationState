ae87d5ac962f148902b39e7e2918f412508dc572
Isaac <23095702+iBeizsley@users.noreply.github.com>
2018-06-14 11:56:08 +0100

    Update build script

--- build.cmd
+++ build.cmd
@@ -3,7 +3,14 @@
 SET VERSION=0.0.0
 IF NOT [%1]==[] (set VERSION=%1)
 
-dotnet test src/ViewModels.ApplicationState.Tests/ViewModels.ApplicationState.Tests.csproj
+curl -o nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+
+.\nuget.exe restore .\src\ViewModels.ApplicationState.Tests\ViewModels.ApplicationState.Tests.csproj -PackagesDirectory .\src\packages -Verbosity detailed
+.\nuget.exe restore .\src\ViewModels.ApplicationState\ViewModels.ApplicationState.csproj -PackagesDirectory .\src\packages -Verbosity detailed
+
+dotnet build .\src\ViewModels.ApplicationState\ViewModels.ApplicationState.csproj -p:Version=%version% -c Release
+
+dotnet test .\src\ViewModels.ApplicationState.Tests\ViewModels.ApplicationState.Tests.csproj
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-dotnet pack src/ViewModels.ApplicationState/ViewModels.ApplicationState.csproj -o ../../dist -p:Version=%version% -p:PackageVersion=%version% -c Release
+.\nuget.exe pack .\src\ViewModels.ApplicationState\ViewModels.ApplicationState.csproj -OutputDirectory ..\..\dist -Version %version% -Verbosity detailed
