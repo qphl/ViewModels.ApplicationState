fb768e66fa40c534e2aadac67f08e8dc13a5ba17
Isaac <23095702+iBeizsley@users.noreply.github.com>
2018-06-26 11:12:06 +0100

    Fail build.cmd if any step returns an error

--- build.cmd
+++ build.cmd
@@ -8,13 +8,20 @@ IF NOT [%2]==[] (set TAG=%2)
 SET TAG=%TAG:tags/=%
 
 curl -o nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -k
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 .\nuget.exe restore .\src\ViewModels.ApplicationState.Tests\ViewModels.ApplicationState.Tests.csproj -PackagesDirectory .\src\packages -Verbosity detailed
+if %errorlevel% neq 0 exit /b %errorlevel%
 .\nuget.exe restore .\src\ViewModels.ApplicationState\ViewModels.ApplicationState.csproj -PackagesDirectory .\src\packages -Verbosity detailed
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 dotnet build .\src\ViewModels.ApplicationState\ViewModels.ApplicationState.csproj -p:Version=%VERSION% -c Release
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 dotnet test .\src\ViewModels.ApplicationState.Tests\ViewModels.ApplicationState.Tests.csproj
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 dotnet pack .\src\ViewModels.ApplicationState\ViewModels.ApplicationState.csproj -o ..\..\dist -p:Version="%VERSION%" -p:PackageVersion="%VERSION%" -p:Tag="%TAG%" -c Release
+exit /b %errorlevel%
